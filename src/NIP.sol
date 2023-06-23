bfa598fc424e20e663616c7222b863c43a6e8929
Ezequiel <eperez@itba.edu.ar>
2023-06-23 15:13:51 -0300

    style(NIP.sol): switch to named imports

--- src/NIP.sol
+++ src/NIP.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "openzeppelin/token/ERC20/ERC20.sol";
-import "openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
-import "openzeppelin/security/Pausable.sol";
-import "openzeppelin/access/AccessControl.sol";
-import "openzeppelin/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
+import {Pausable} from "openzeppelin/security/Pausable.sol";
+import {AccessControl} from "openzeppelin/access/AccessControl.sol";
+import {ERC20Permit} from "openzeppelin/token/ERC20/extensions/ERC20Permit.sol";
 
 contract NIP is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Permit {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");

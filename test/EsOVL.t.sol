c9a0e3562e4c770401f1e9503ffc705466a4adb9
Ezequiel <eperez@itba.edu.ar>
2023-06-28 11:43:04 -0300

    feat: NIP deploy script and downgrade solidity version to 0.8.19

--- test/EsOVL.t.sol
+++ test/EsOVL.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 import {EsOVL} from "../src/EsOVL.sol";
@@ -10,70 +10,97 @@ contract EsOVLTest is Test {
 
     OVL public ovl;
 
-    address public constant OWNER = address(0x1);
+    address public OWNER;
     address public constant USER = address(0x2);
-    uint256 public constant TOTAL_SUPPLY = 1000;
-    uint256 public constant RELEASE_TIMESTAMP = 6123215;
+    uint256 public constant AMOUNT = 1000;
+    uint64 public constant START = 1687877663;
+    uint64 public constant DURATION = 30*24*60*60; // 30 days
+
+    event VestingPeriodUpdated(uint64 start, uint64 duration);
+    event ExcessOVLWithdrawn(address to, uint256 amount);
+    event OVLRedeemed(address indexed to, uint256 amount);
 
     function setUp() public {
-        vm.startPrank(OWNER);
+        OWNER = address(this);
 
         ovl = new OVL();
-        esOVL = new EsOVL(ovl, RELEASE_TIMESTAMP);
+        esOVL = new EsOVL(ovl, START, DURATION);
 
-        assertEq(esOVL.owner(), OWNER);
+        assertTrue(esOVL.hasRole(esOVL.DEFAULT_ADMIN_ROLE(), OWNER));
+        assertEq(esOVL.end(), START + DURATION);
 
-        deal(address(ovl), OWNER, TOTAL_SUPPLY);
-        ovl.approve(address(esOVL), TOTAL_SUPPLY);
-        esOVL.mint(TOTAL_SUPPLY);
+        deal(address(ovl), OWNER, AMOUNT);
+        ovl.approve(address(esOVL), AMOUNT);
     }
 
-    function testMint() public {
-        // OWNER mints TOTAL_SUPPLY in the setUp function
-        assertEq(esOVL.totalSupply(), TOTAL_SUPPLY);
-        assertEq(esOVL.balanceOf(OWNER), TOTAL_SUPPLY);
-        assertEq(ovl.balanceOf(address(esOVL)), TOTAL_SUPPLY);
+    function testMintTo() public {
+        vm.expectRevert(EsOVL.TransferToZeroAddress.selector);
+        esOVL.mintTo(address(0), AMOUNT);
+
+        esOVL.mintTo(USER, AMOUNT);
+        assertEq(esOVL.totalSupply(), AMOUNT);
+        assertEq(esOVL.balanceOf(USER), AMOUNT);
+        assertEq(ovl.balanceOf(address(esOVL)), AMOUNT);
+        assertEq(ovl.balanceOf(OWNER), 0);
+    }
+
+    function testUpdateVestingPeriod() public {
+        uint64 newStart = START + 1000;
+        uint64 newDuration = DURATION * 2;
+
+        vm.expectEmit();
+        emit VestingPeriodUpdated(newStart, newDuration);
+        esOVL.updateVestingPeriod(newStart, newDuration);
+        assertEq(esOVL.end(), newStart + newDuration);
+
+        vm.startPrank(USER);
+        vm.expectRevert(); // user does not have DEFAULT_ADMIN_ROLE
+        esOVL.updateVestingPeriod(newStart, newDuration);
+    }
+
+    function testWithdrawExcessOVL() public {
+        uint256 excess = 100;
+        deal(address(ovl), address(esOVL), excess);
+
+        esOVL.mintTo(OWNER, AMOUNT);
         assertEq(ovl.balanceOf(OWNER), 0);
 
-        changePrank(USER);
-        vm.expectRevert();
-        esOVL.mint(TOTAL_SUPPLY);
+        vm.expectEmit();
+        emit ExcessOVLWithdrawn(OWNER, excess);
+        esOVL.withdrawExcessOVL();
+        assertEq(ovl.balanceOf(OWNER), excess);
+        assertEq(ovl.balanceOf(address(esOVL)), AMOUNT);
+
+        vm.startPrank(USER);
+        vm.expectRevert(); // user does not have DEFAULT_ADMIN_ROLE
+        esOVL.withdrawExcessOVL();
     }
 
-    function testUpdateReleaseTimestamp() public {
-        uint256 newTimestamp = 2*RELEASE_TIMESTAMP;
-        esOVL.updateReleaseTimestamp(newTimestamp);
-        assertEq(esOVL.releaseTimestamp(), newTimestamp);
+    // TODO: adapt to linear vesting
+    // function testRedeem() public {
+    //     vm.warp(RELEASE_TIMESTAMP - 1);
+
+    //     uint256 userBalance = 100;
+    //     esOVL.transfer(USER, userBalance);
+
+    //     vm.expectRevert(abi.encodePacked(
+    //         EsOVL.TimestampNotReached.selector,
+    //         RELEASE_TIMESTAMP,
+    //         RELEASE_TIMESTAMP - 1
+    //     ));
+    //     esOVL.redeem();
+
+    //     vm.warp(RELEASE_TIMESTAMP);
         
-        changePrank(USER);
-        vm.expectRevert();
-        esOVL.updateReleaseTimestamp(newTimestamp);
-    }
-
-    function testRedeem() public {
-        vm.warp(RELEASE_TIMESTAMP - 1);
-
-        uint256 userBalance = 100;
-        esOVL.transfer(USER, userBalance);
-
-        vm.expectRevert(abi.encodePacked(
-            EsOVL.TimestampNotReached.selector,
-            RELEASE_TIMESTAMP,
-            RELEASE_TIMESTAMP - 1
-        ));
-        esOVL.redeem();
-
-        vm.warp(RELEASE_TIMESTAMP);
+    //     esOVL.redeem();
+    //     assertEq(esOVL.balanceOf(OWNER), 0);
         
-        esOVL.redeem();
-        assertEq(esOVL.balanceOf(OWNER), 0);
-        
-        changePrank(USER);
-        esOVL.redeem();
-        assertEq(ovl.balanceOf(USER), userBalance);
-        assertEq(esOVL.balanceOf(USER), 0);
+    //     changePrank(USER);
+    //     esOVL.redeem();
+    //     assertEq(ovl.balanceOf(USER), userBalance);
+    //     assertEq(esOVL.balanceOf(USER), 0);
 
-        assertEq(ovl.balanceOf(address(esOVL)), 0);
-        assertEq(esOVL.totalSupply(), 0);
-    }
+    //     assertEq(ovl.balanceOf(address(esOVL)), 0);
+    //     assertEq(esOVL.totalSupply(), 0);
+    // }
 }
